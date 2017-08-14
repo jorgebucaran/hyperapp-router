358a5b7c2f8b216ccd9ea5630df37a438c721211
Jorge Bucaran <jbucaran@gmail.com>
2017-08-14 23:05:29 +0900

    Update router to hyperapp's latest.
    - No breaking changes.
    - Add new .index property to route info object.

--- test/router.test.js
+++ test/router.test.js
@@ -1,10 +1,7 @@
 import { h, app } from "hyperapp"
 import { Router } from "../src"
 
-const expectHTMLToBe = (body, ...values) =>
-  expect(document.body.innerHTML).toBe(
-    body.reduce((a, b, i) => a + values[i - 1] + b).replace(/\s{2,}/g, "")
-  )
+window.requestAnimationFrame = setTimeout
 
 Object.defineProperty(window.location, "pathname", {
   writable: true
@@ -13,65 +10,88 @@ Object.defineProperty(window.location, "pathname", {
 beforeEach(() => {
   document.body.innerHTML = ""
   location.pathname = "/"
-  history.pushState = Function.prototype
+  history.pushState = function(state, title, url) {
+    location.pathname = url
+  }
 })
 
-test("/", () => {
+test("/", done => {
   app({
-    view: [["/", state => h("div", {}, "foo")]],
+    view: [
+      [
+        "/",
+        state =>
+          h(
+            "div",
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(`<div>foo</div>`)
+                done()
+              }
+            },
+            "foo"
+          )
+      ]
+    ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <div>
-      foo
-    </div>`
 })
 
-test("*", () => {
+test("*", done => {
+  location.pathname = "/foo"
+
   app({
-    view: [["*", state => h("div", {}, "foo")]],
-    mixins: [Router],
-    events: {
-      loaded: (state, actions) => {
-        actions.router.go("/bar")
-        expectHTMLToBe`
-          <div>
-            foo
-          </div>`
-
-        actions.router.go("/baz")
-        expectHTMLToBe`
-          <div>
-            foo
-          </div>`
-
-        actions.router.go("/")
-        expectHTMLToBe`
-          <div>
-            foo
-          </div>`
-      }
-    }
+    view: [
+      [
+        "*",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(`<div>foo</div>`)
+                actions.router.go("/bar")
+              },
+              onupdate() {
+                expect(document.body.innerHTML).toBe(`<div>foo</div>`)
+                done()
+              }
+            },
+            "foo"
+          )
+      ]
+    ],
+    mixins: [Router]
   })
 })
 
-test("routes", () => {
+test("routes", done => {
   window.location.pathname = "/foo/bar/baz"
 
   app({
-    view: [["/foo/bar/baz", state => h("div", {}, "foo", "bar", "baz")]],
+    view: [
+      [
+        "/foo/bar/baz",
+        state =>
+          h(
+            "div",
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(`<div>foobarbaz</div>`)
+                done()
+              }
+            },
+            "foo",
+            "bar",
+            "baz"
+          )
+      ]
+    ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <div>
-      foobarbaz
-    </div>
-  `
 })
 
-test("route params", () => {
+test("route params", done => {
   window.location.pathname = "/be_ep/bOp/b00p"
 
   app({
@@ -81,7 +101,14 @@ test("route params", () => {
         state =>
           h(
             "ul",
-            {},
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(
+                  `<ul><li>foo:be_ep</li><li>bar:bOp</li><li>baz:b00p</li></ul>`
+                )
+                done()
+              }
+            },
             Object.keys(state.router.params).map(key =>
               h("li", {}, `${key}:${state.router.params[key]}`)
             )
@@ -90,17 +117,9 @@ test("route params", () => {
     ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <ul>
-      <li>foo:be_ep</li>
-      <li>bar:bOp</li>
-      <li>baz:b00p</li>
-    </ul>
-  `
 })
 
-test("route params separated by a dash", () => {
+test("route params separated by a dash", done => {
   window.location.pathname = "/beep-bop-boop"
 
   app({
@@ -110,7 +129,14 @@ test("route params separated by a dash", () => {
         state =>
           h(
             "ul",
-            {},
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(
+                  `<ul><li>foo:beep</li><li>bar:bop</li><li>baz:boop</li></ul>`
+                )
+                done()
+              }
+            },
             Object.keys(state.router.params).map(key =>
               h("li", {}, `${key}:${state.router.params[key]}`)
             )
@@ -119,17 +145,9 @@ test("route params separated by a dash", () => {
     ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <ul>
-      <li>foo:beep</li>
-      <li>bar:bop</li>
-      <li>baz:boop</li>
-    </ul>
-  `
 })
 
-test("route params including a dot", () => {
+test("route params including a dot", done => {
   window.location.pathname = "/beep/bop.bop/boop"
 
   app({
@@ -139,7 +157,14 @@ test("route params including a dot", () => {
         state =>
           h(
             "ul",
-            {},
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(
+                  `<ul><li>foo:beep</li><li>bar:bop.bop</li><li>baz:boop</li></ul>`
+                )
+                done()
+              }
+            },
             Object.keys(state.router.params).map(key =>
               h("li", {}, `${key}:${state.router.params[key]}`)
             )
@@ -148,85 +173,208 @@ test("route params including a dot", () => {
     ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <ul>
-			<li>foo:beep</li>
-      <li>bar:bop.bop</li>
-      <li>baz:boop</li>
-    </ul>
-  `
 })
 
-test("routes with dashes into a single param key", () => {
+test("routes with dashes into a single param key", done => {
   window.location.pathname = "/beep-bop-boop"
 
   app({
-    view: [["/:foo", state => h("div", {}, state.router.params.foo)]],
+    view: [
+      [
+        "/:foo",
+        state =>
+          h(
+            "div",
+            {
+              oncreate() {
+                expect(document.body.innerHTML).toBe(`<div>beep-bop-boop</div>`)
+                done()
+              }
+            },
+            state.router.params.foo
+          )
+      ]
+    ],
     mixins: [Router]
   })
-
-  expectHTMLToBe`
-    <div>
-      beep-bop-boop
-    </div>
-  `
 })
 
-test("popstate", () => {
+test("popstate", done => {
+  const view = name =>
+    h(
+      "div",
+      {
+        oncreate() {
+          expect(document.body.innerHTML).toBe(`<div>${name}</div>`)
+
+          const event = document.createEvent("Event")
+          event.initEvent("popstate", true, true)
+
+          window.location.pathname = `/foo`
+          window.document.dispatchEvent(event)
+        },
+        onupdate() {
+          expect(document.body.innerHTML).toBe(`<div>${name}</div>`)
+          done()
+        }
+      },
+      name
+    )
+
   app({
-    view: [["/", state => ""], ["/foo", state => h("div", {}, "foo")]],
+    view: [["/", () => view("")], ["/foo", () => view("foo")]],
     mixins: [Router]
   })
-
-  window.location.pathname = "/foo"
-
-  const event = document.createEvent("Event")
-  event.initEvent("popstate", true, true)
-  window.document.dispatchEvent(event)
-
-  expectHTMLToBe`
-    <div>
-      foo
-    </div>
-  `
 })
 
-test("go", () => {
-  window.history.pushState = (data, title, url) =>
-    expect(url).toMatch(/^\/(foo|bar|baz)$/)
-
+test("go", done => {
   app({
     view: [
-      ["/", state => ""],
-      ["/foo", state => h("div", {}, "foo")],
-      ["/bar", state => h("div", {}, "bar")],
-      ["/baz", state => h("div", {}, "baz")]
+      [
+        "/",
+        (state, actions) =>
+          h("div", {
+            oncreate() {
+              expect(document.body.innerHTML).toBe("<div></div>")
+              actions.router.go("/foo")
+            }
+          })
+      ],
+      [
+        "/foo",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              onupdate() {
+                expect(document.body.innerHTML).toBe("<div>foo</div>")
+                actions.router.go("/bar")
+              }
+            },
+            "foo"
+          )
+      ],
+      [
+        "/bar",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              onupdate() {
+                expect(document.body.innerHTML).toBe("<div>bar</div>")
+                actions.router.go("/baz")
+              }
+            },
+            "bar"
+          )
+      ],
+      [
+        "/baz",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              onupdate() {
+                expect(document.body.innerHTML).toBe("<div>baz</div>")
+                done()
+              }
+            },
+            "baz"
+          )
+      ]
     ],
-    mixins: [Router],
+    mixins: [Router]
+  })
+})
+
+test("route", done => {
+  app({
     events: {
-      loaded: (state, actions) => {
-        actions.router.go("/foo")
-        expectHTMLToBe`
-          <div>
-            foo
-          </div>
-        `
-
-        actions.router.go("/bar")
-        expectHTMLToBe`
-          <div>
-            bar
-          </div>
-        `
-
-        actions.router.go("/baz")
-        expectHTMLToBe`
-          <div>
-            baz
-          </div>
-        `
+      route(state, actions, route) {
+        expect(route.match).toBe("/")
+        done()
       }
-    }
+    },
+    view: [["/", (state, actions) => h("div", {}, "foo")]],
+    mixins: [Router]
+  })
+})
+
+test("do not fire route for unrelated state updates", done => {
+  app({
+    state: {
+      value: "foo"
+    },
+    actions: {
+      bar() {
+        return {
+          value: "bar"
+        }
+      }
+    },
+    events: {
+      route(state, actions, route) {
+        expect(state.value).toBe("foo")
+      }
+    },
+    view: [
+      [
+        "/",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              oncreate() {
+                actions.bar()
+              },
+              onupdate() {
+                done()
+              }
+            },
+            "foo"
+          )
+      ]
+    ],
+    mixins: [Router]
+  })
+})
+
+test("fire route only if path changes", done => {
+  app({
+    state: {
+      value: "foo"
+    },
+    actions: {
+      bar() {
+        return {
+          value: "bar"
+        }
+      }
+    },
+    events: {
+      route(state, actions, route) {
+        expect(state.value).toBe("foo")
+      }
+    },
+    view: [
+      [
+        "/",
+        (state, actions) =>
+          h(
+            "div",
+            {
+              oncreate() {
+                actions.bar()
+                actions.router.go("/")
+              },
+              onupdate() {
+                done()
+              }
+            },
+            "foo"
+          )
+      ]
+    ],
+    mixins: [Router]
   })
 })
