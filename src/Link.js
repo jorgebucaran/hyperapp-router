69878e54215072a96b88757c6eaf018db7f6c50c
Mike Montoya <13255599+SkaterDad@users.noreply.github.com>
2017-08-21 17:50:34 +0000

    Link Component Enhancements
    Click handler should only prevent default behavior if left-click/enter key is used and no modifier keys are pressed.  This is common among most other libraries.

--- src/Link.js
+++ src/Link.js
@@ -4,9 +4,13 @@ export function Link(props, children) {
   props.href = props.to
   props.to = null
 
-  props.onclick = function(element) {
-    element.preventDefault()
-    props.go(props.href)
+  props.onclick = function(e) {
+    if (e.metaKey || e.altKey || e.ctrlKey || e.shiftKey) return
+
+    if (e.button === 0) {
+      e.preventDefault()
+      props.go(props.href)
+    }
   }
 
   return h("a", props, children)
--- test/link.test.js
+++ test/link.test.js
@@ -1,7 +1,7 @@
 import { h } from "hyperapp"
 import { Link } from "../src"
 
-test("/", done => {
+test("Link", done => {
   const link = h(Link, {
     to: "foo",
     go(path) {
@@ -13,6 +13,97 @@ test("/", done => {
   expect(link.data.href).toBe("foo")
 
   link.data.onclick({
+    button: 0, // Left click
     preventDefault() {} // Noop
   })
 })
+
+test("Link - Ignore middle click", done => {
+  const mock = jest.fn()
+
+  const link = h(Link, {
+    to: "foo",
+    go: mock
+  })
+
+  link.data.onclick({
+    button: 1, // Not left click
+    preventDefault() {} // Noop
+  })
+
+  expect(mock.mock.calls.length).toBe(0)
+  done()
+})
+
+test("Link - Ignore if Meta key", done => {
+  const mock = jest.fn()
+
+  const link = h(Link, {
+    to: "foo",
+    go: mock
+  })
+
+  link.data.onclick({
+    button: 0, // Left click
+    metaKey: true,
+    preventDefault() {} // Noop
+  })
+
+  expect(mock.mock.calls.length).toBe(0)
+  done()
+})
+
+test("Link - Ignore if CTRL key", done => {
+  const mock = jest.fn()
+
+  const link = h(Link, {
+    to: "foo",
+    go: mock
+  })
+
+  link.data.onclick({
+    button: 0, // Left click
+    ctrlKey: true,
+    preventDefault() {} // Noop
+  })
+
+  expect(mock.mock.calls.length).toBe(0)
+  done()
+})
+
+test("Link - Ignore if ALT key", done => {
+  const mock = jest.fn()
+
+  const link = h(Link, {
+    to: "foo",
+    go: mock
+  })
+
+  link.data.onclick({
+    button: 0, // Left click
+    altKey: true,
+    preventDefault() {} // Noop
+  })
+
+  expect(mock.mock.calls.length).toBe(0)
+  done()
+})
+
+test("Link - Ignore if Shift key", done => {
+  const mock = jest.fn()
+
+  const link = h(Link, {
+    to: "foo",
+    go: mock
+  })
+
+  link.data.onclick({
+    button: 0, // Left click
+    shiftKey: true,
+    preventDefault() {} // Noop
+  })
+
+  expect(mock.mock.calls.length).toBe(0)
+  done()
+})
+
