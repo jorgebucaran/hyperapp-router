4539d2d68e3c0ec27816693b605fd7cd7b834d07
Jorge Bucaran <mail@jorgebucaran.com>
2017-12-03 21:54:19 +0900

    Let customize underlying <a> element in Link component.

--- src/Link.js
+++ src/Link.js
@@ -1,32 +1,28 @@
 import { h } from "hyperapp"
 
 export function Link(props, children) {
-  var href = props.to
+  var to = props.to
   var location = props.location || window.location
 
-  return h(
-    "a",
-    {
-      href: href,
-      onclick: function(e) {
-        if (
-          e.button !== 0 ||
-          e.altKey ||
-          e.metaKey ||
-          e.ctrlKey ||
-          e.shiftKey ||
-          props.target === "_blank" ||
-          e.currentTarget.origin !== location.origin
-        ) {
-        } else {
-          e.preventDefault()
+  props.href = to
+  props.onclick = function(e) {
+    if (
+      e.button !== 0 ||
+      e.altKey ||
+      e.metaKey ||
+      e.ctrlKey ||
+      e.shiftKey ||
+      props.target === "_blank" ||
+      e.currentTarget.origin !== location.origin
+    ) {
+    } else {
+      e.preventDefault()
 
-          if (location.pathname !== href) {
-            history.pushState(location.pathname, "", href)
-          }
-        }
+      if (location.pathname !== to) {
+        history.pushState(location.pathname, "", to)
       }
-    },
-    children
-  )
+    }
+  }
+
+  return h("a", props, children)
 }
