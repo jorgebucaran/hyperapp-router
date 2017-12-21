d93b78c7e06a02ab82a00050de9b27f15e2b2c81
Vladimir Sabev <vdsabev@gmail.com>
2017-12-21 16:33:59 +0200

    Allow using Link without Hyperapp

--- src/Link.js
+++ src/Link.js
@@ -1,28 +1,4 @@
 import { h } from "hyperapp"
+import { createLink } from "./createLink"
 
-export function Link(props, children) {
-  var to = props.to
-  var location = props.location || window.location
-
-  props.href = to
-  props.onclick = function(e) {
-    if (
-      e.button !== 0 ||
-      e.altKey ||
-      e.metaKey ||
-      e.ctrlKey ||
-      e.shiftKey ||
-      props.target === "_blank" ||
-      e.currentTarget.origin !== location.origin
-    ) {
-    } else {
-      e.preventDefault()
-
-      if (to !== location.pathname) {
-        history.pushState(location.pathname, "", to)
-      }
-    }
-  }
-
-  return h("a", props, children)
-}
+export var Link = createLink(h);
---
+++ src/createLink.js
@@ -0,0 +1,28 @@
+export function createLink(h) {
+  return function(props, children) {
+    var to = props.to
+    var location = props.location || window.location
+
+    props.href = to
+    props.onclick = function(e) {
+      if (
+        e.button !== 0 ||
+        e.altKey ||
+        e.metaKey ||
+        e.ctrlKey ||
+        e.shiftKey ||
+        props.target === "_blank" ||
+        e.currentTarget.origin !== location.origin
+      ) {
+      } else {
+        e.preventDefault()
+
+        if (to !== location.pathname) {
+          history.pushState(location.pathname, "", to)
+        }
+      }
+    }
+
+    return h("a", props, children)
+  }
+}
