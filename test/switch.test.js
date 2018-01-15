a1538739bc7f72fbd46d85e4a4221be2fe06ab4c
Zacharias Enochsson <zache@axis.com>
2018-01-15 21:32:58 +0100

    fix code style

--- test/switch.test.js
+++ test/switch.test.js
@@ -1,13 +1,13 @@
 import { Switch } from "../src/Switch"
 
-test('Switch returns only the first truthy child', () => {
-  const expected = 'truthy value'
-  const children = [0, null, expected, false, 'other']
+test("Switch returns only the first truthy child", () => {
+  const expected = "truthy value"
+  const children = [0, null, expected, false, "other"]
   expect(Switch(null, children)).toBe(expected)
 })
 
-test('Switch returns falsy when all children falsy', () => {
-  const children = [0, '', false, null]
+test("Switch returns falsy when all children falsy", () => {
+  const children = [0, "", false, null]
   expect(Switch(null, children)).toBeFalsy()
 })
 
