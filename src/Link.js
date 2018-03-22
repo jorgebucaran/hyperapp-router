1365eca366b89402b5b1d0ac52062088e9f7b8c0
Swizz <gerodel.quentin@gmail.com>
2018-03-22 15:56:57 +0100

    Use Lazy Components features

--- src/Link.js
+++ src/Link.js
@@ -1,28 +1,30 @@
 import { h } from "hyperapp"
 
 export function Link(props, children) {
-  var to = props.to
-  var location = props.location || window.location
+  return function(state, actions) {
+    var to = props.to
+    var location = state.location || window.location
 
-  props.href = to
-  props.onclick = function(e) {
-    if (
-      e.button !== 0 ||
-      e.altKey ||
-      e.metaKey ||
-      e.ctrlKey ||
-      e.shiftKey ||
-      props.target === "_blank" ||
-      e.currentTarget.origin !== location.origin
-    ) {
-    } else {
-      e.preventDefault()
+    props.href = to
+    props.onclick = function(e) {
+      if (
+        e.button !== 0 ||
+        e.altKey ||
+        e.metaKey ||
+        e.ctrlKey ||
+        e.shiftKey ||
+        props.target === "_blank" ||
+        location.origin && e.currentTarget.origin !== location.origin
+      ) {
+      } else {
+        e.preventDefault()
 
-      if (to !== location.pathname) {
-        history.pushState(location.pathname, "", to)
+        if (to !== location.pathname) {
+          history.pushState(location.pathname, "", to)
+        }
       }
     }
-  }
 
-  return h("a", props, children)
+    return h("a", props, children)
+  }
 }
