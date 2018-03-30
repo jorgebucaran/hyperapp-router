fc054518b4a751ed8a311f577ce6985dc501152a
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-03-30 19:47:01 +0900

    Fix Switch to handle lazy components

--- src/Switch.js
+++ src/Switch.js
@@ -1,5 +1,12 @@
 export function Switch(props, children) {
-  var i = 0
-  while (!children[i] && i < children.length) i++
-  return children[i]
+  return function(state, actions) {
+    var child,
+      i = 0
+    while (
+      !(child = children[i] && children[i](state, actions)) &&
+      i < children.length
+    )
+      i++
+    return child
+  }
 }
--- test/switch.test.js
+++ test/switch.test.js
@@ -2,11 +2,15 @@ import { Switch } from "../src/Switch"
 
 test("Switch returns only the first truthy child", () => {
   const expected = "truthy value"
-  const children = [0, null, expected, false, "other"]
-  expect(Switch(null, children)).toBe(expected)
+  const children = [0, null, expected, false, "other"].map(child => () => child)
+  expect(Switch(null, children)()).toBe(expected)
 })
 
 test("Switch returns falsy when all children falsy", () => {
-  const children = [0, "", false, null]
-  expect(Switch(null, children)).toBeFalsy()
+  const children = [0, "", false, null].map(child => () => child)
+  expect(Switch(null, children)()).toBeFalsy()
+})
+
+test("Switch returns falsy when children is empty", () => {
+  expect(Switch(null, [])()).toBeFalsy()
 })
