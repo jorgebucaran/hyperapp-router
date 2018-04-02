218a31a2ef3711aa3fc57b7d68f93829169cb4aa
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-04-02 22:08:05 +0900

    Improve tests by distinguishing between integration tests and unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,38 +1,67 @@
 import { h, app } from "hyperapp"
-import { Route, location } from "../src"
+import { Route, Link, Redirect, location } from "../src"
 
-test("Router", done => {
-  const state = {
-    location: location.state
-  }
+const wait = async ms => new Promise(resolve => setTimeout(resolve, ms))
 
-  const actions = {
-    location: location.actions
-  }
+let state, actions, unsubscribe
 
-  const main = app(
-    state,
-    actions,
-    (state, actions) => (
-      <Route
-        path="/test"
-        render={() => (
-          <h1
-            oncreate={() => {
-              expect(document.body.innerHTML).toBe(`<h1>Hello</h1>`)
-              unsubscribe()
-              done()
-            }}
-          >
-            Hello
-          </h1>
-        )}
-      />
-    ),
-    document.body
-  )
+beforeEach(() => {
+  state = { location: location.state }
+  actions = { location: location.actions }
+  unsubscribe = null
+})
 
-  const unsubscribe = location.subscribe(main.location)
+afterEach(() => {
+  unsubscribe && unsubscribe()
+})
 
+test("Transition by location.go()", async done => {
+  const spy = jest.fn()
+  const view = () => <Route path="/test" render={spy} />
+  const main = app(state, actions, view, document.body)
+  unsubscribe = location.subscribe(main.location)
+  await wait(0)
+  expect(spy).not.toBeCalled()
   main.location.go("/test")
+  await wait(0)
+  expect(spy).toBeCalled()
+  done()
+})
+
+test("Transition by clicking Link", async done => {
+  const spy = jest.fn()
+  const view = () => (
+    <div>
+      <Link to="/test" />
+      <Route path="/test" render={spy} />
+    </div>
+  )
+  const main = app(state, actions, view, document.body)
+  unsubscribe = location.subscribe(main.location)
+  await wait(0)
+  expect(spy).not.toBeCalled()
+  document.body
+    .getElementsByTagName("a")[0]
+    .dispatchEvent(new MouseEvent("click", { button: 0 }))
+  await wait(0)
+  expect(spy).toBeCalled()
+  done()
+})
+
+test("Transition by rendering Redirect", async done => {
+  const spy = jest.fn()
+  const view = () => (
+    <div>
+      <Route path="/test" render={() => <Redirect to="/somewhere" />} />
+      <Route path="/somewhere" render={spy} />
+    </div>
+  )
+  const main = app(state, actions, view, document.body)
+  unsubscribe = location.subscribe(main.location)
+  await wait(0)
+  expect(spy).not.toBeCalled()
+  main.location.go("/test")
+  await wait(0)
+  expect(spy).toBeCalled()
+  done()
 })
--- test/link.test.js
+++ test/link.test.js
@@ -1,67 +1,18 @@
-import { h, app } from "hyperapp"
-import { Route, Link, location } from "../src"
+import { Link } from "../src"
 
-test("Link", done => {
-  const state = {
-    location: location.state
-  }
-
-  const actions = {
-    location: location.actions
-  }
-
-  const main = app(
-    state,
-    actions,
-    (state, actions) => (
-      <div>
-        <Route path="/test" render={() => <Link to="/done" />} />
-        <Route
-          path="/done"
-          render={() => (
-            <h1
-              oncreate={() => {
-                expect(document.body.innerHTML).toBe(
-                  `<div><h1>Hello</h1></div>`
-                )
-                unsubscribe()
-                done()
-              }}
-            >
-              Hello
-            </h1>
-          )}
-        />
-      </div>
-    ),
-    document.body
-  )
-
-  const unsubscribe = location.subscribe(main.location)
-
-  main.location.go("/test")
-
-  setTimeout(() => {
-    document
-      .getElementsByTagName("a")[0]
-      .dispatchEvent(new MouseEvent("click", { button: 0 }))
-  }, 0)
-})
-
-test("pass through attributes", () => {
-  const vnode = h(Link, { to: "/path", pass: "through", location })({}, {})
+test('Link passes given attributes except "to" and "location" to underlying element', () => {
+  const vnode = Link({ to: "/path", pass: "through", location })({}, {})
   expect(vnode.attributes.to).toBeUndefined()
   expect(vnode.attributes.location).toBeUndefined()
   expect(vnode.attributes.href).toEqual("/path")
   expect(vnode.attributes.pass).toEqual("through")
 })
 
-test("custom onclick handler", () => {
-  const event = { defaultPrevented: false }
-  const onclick = e => {
-    e.defaultPrevented = true
-  }
-  const vnode = h(Link, { to: "/path", onclick })({}, {})
+test("Calling onclick of VNode transparently calls Link's onclick prop", () => {
+  const onclickProp = jest.fn()
+  const vnode = Link({ onclick: onclickProp })({}, {})
+  const event = new Object()
+  expect(vnode.attributes.onclick).not.toBe(onclickProp)
   vnode.attributes.onclick(event)
-  expect(event.defaultPrevented).toBe(true)
+  expect(onclickProp).toBeCalledWith(event)
 })
