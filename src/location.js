09eeb7fcc5d5d2d89cd454adb056a30d6d6851c0
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-04-18 19:50:15 +0900

    Polyfill CustomEvent if necessary

--- src/location.js
+++ src/location.js
@@ -1,3 +1,16 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent#Polyfill
+var CustomEvent = (function (c) {
+  if (typeof c === "function") return c
+  function CustomEvent(event, params) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+  }
+  CustomEvent.prototype = window.Event.prototype
+  return CustomEvent
+})(window.CustomEvent)
+
 function wrapHistory(keys) {
   return keys.reduce(function(next, key) {
     var fn = history[key]
