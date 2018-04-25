427ade711cf98d19e9e848c2c98177d1a3c98ffa
Jairo Caro-Accino Viciana <jairo@galax.be>
2018-04-25 18:29:44 +0200

    BasePath

--- src/Route.js
+++ src/Route.js
@@ -2,7 +2,19 @@ import { parseRoute } from "./parseRoute"
 
 export function Route(props) {
   return function(state, actions) {
-    var location = state.location
+    var location = state.location;
+    // Basepath must end with /
+    if(props.basepath && props.basepath[props.basepath.length] !== "/"){
+      props.basepath += "/";
+    }
+    // Remove basepath from path
+    if(~props.path.indexOf(props.basepath)){
+      props.path = props.path.split(props.basepath)[1];
+    }
+    // Remove basepath from url
+    if(~location.pathname.indexOf(props.basepath)){
+      location.pathname = location.pathname.split(props.basepath)[1];
+    }
     var match = parseRoute(props.path, location.pathname, {
       exact: !props.parent
     })
