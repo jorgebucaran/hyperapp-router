ba2cd358820e7253c451d27fc3d8865714929ea8
Swizz <gerodel.quentin@gmail.com>
2018-04-26 14:58:44 +0200

    Render from first children instead of render props

--- src/Route.js
+++ src/Route.js
@@ -1,6 +1,6 @@
 import { parseRoute } from "./parseRoute"
 
-export function Route(props) {
+export function Route(props, children) {
   return function(state, actions) {
     var location = state.location
     var match = parseRoute(props.path, location.pathname, {
@@ -9,7 +9,7 @@ export function Route(props) {
 
     return (
       match &&
-      props.render({
+      children[0]({
         match: match,
         location: location
       })
