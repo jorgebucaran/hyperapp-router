1586bfaf840bfbeb89cd6cf2829e33c0c5be0527
atomita <sleeping.cait.sith+gh@gmail.com>
2018-05-12 18:16:08 +0900

    fix: wrapHistory function
    `new CustomEvent` is not supported by IE

--- src/location.js
+++ src/location.js
@@ -4,7 +4,7 @@ function wrapHistory(keys) {
 
     history[key] = function(data, title, url) {
       fn.call(this, data, title, url)
-      dispatchEvent(new CustomEvent("pushstate", { detail: data }))
+      dispatchEvent(createCustomEvent("pushstate", data))
     }
 
     return function() {
@@ -13,6 +13,17 @@ function wrapHistory(keys) {
     }
   }, null)
 }
+
+var createCustomEvent = "object" === typeof CustomEvent
+    ? function (target, data) {
+      // `new CustomEvent` is not supported by IE
+      var event  = document.createEvent("CustomEvent")
+      event.initCustomEvent(target, false, false, data)
+      return event
+    }
+    : function (target, data) {
+      return new CustomEvent(target, { detail: data })
+    }
 
 export var location = {
   state: {
